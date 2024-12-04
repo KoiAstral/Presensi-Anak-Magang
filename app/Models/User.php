bb577b4be0b42e08c80d1f85730e7b48a2763166
Riyan Dwi Cahyadi <114849739+RiyanAokii1@users.noreply.github.com>
2024-12-04 10:16:05 +0800

    fixed Clogin, user in models

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'role',
+        'nama',
+        'nisn',
         'password',
     ];
 
@@ -42,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getAuthIdentifierName()
+    {
+        return 'nisn';
+    }
 }
