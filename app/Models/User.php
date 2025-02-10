0738cb1a7cf356e15014788926854a6a9e7611e5
Riyan Dwi Cahyadi <114849739+RiyanAokii1@users.noreply.github.com>
2025-02-10 20:23:01 +0800

    "add password and sekolah to migration, Models"

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'nama',
+        'sekolah',
         'nomor_induk',
         'email',
-        'status'
+        'status',
+        'password'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         $table->id();
         $table->string('nama');
         $table->string('nomor_induk')->unique(); // Add unique index here
+        $table->string('sekolah'); // Add unique index here
         $table->string('email')->unique();
         $table->timestamp('email_verified_at')->nullable();
         $table->string('password');
