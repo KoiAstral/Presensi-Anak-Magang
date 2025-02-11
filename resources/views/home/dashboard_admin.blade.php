ca1946bcf7a68dc78198c650e6fc71a636567be7
Riyan Dwi Cahyadi <114849739+RiyanAokii1@users.noreply.github.com>
2025-02-11 20:13:38 +0800

    fix url redirect to login when press logout

--- resources/views/home/dashboard_admin.blade.php
+++ resources/views/home/dashboard_admin.blade.php
@@ -63,7 +63,7 @@
                 <p class="mb-4">Are you sure you want to logout?</p>
                 <div class="flex justify-end">
                     <button id="cancelLogout" class="mr-2 px-4 py-2 bg-gray-200 rounded">Cancel</button>
-                    <a href="/login" class="px-4 py-2 bg-red-500 text-white rounded">Logout</a>
+                    <a href="/logout" class="px-4 py-2 bg-red-500 text-white rounded">Logout</a>
                 </div>
             </div>
         </div>
