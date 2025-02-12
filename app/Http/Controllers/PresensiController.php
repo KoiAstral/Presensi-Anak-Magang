a533848ab275eb548c8de9c989ca020243fb4dbe
Riyan Dwi Cahyadi <114849739+RiyanAokii1@users.noreply.github.com>
2025-02-12 18:37:22 +0800

    restore the store function

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Presensi;
-use App\Http\Controllers\UserController;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class PresensiController extends Controller
 {
     public function indexAdmin()
     {
         $presensi = Presensi::all();
-
         return view('admin.presensi.data_magang', compact('presensi'));
+    }
+
+    public function store()
+    {
+        $presensi = new Presensi();
+        $presensi->nomor_induk = Auth::user()->nomor_induk;
+        $presensi->tanggal_presensi = Carbon::now()->toDateString();
+        $presensi->waktu_presensi = Carbon::now()->toTimeString();
+        $presensi->status = 'Hadir';
+        $presensi->save();
+
+        return redirect()->back()->with('success', 'Presensi berhasil disimpan!');
     }
 }
