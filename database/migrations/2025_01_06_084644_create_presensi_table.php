63cf910846c300be511f5087832c7ad333c43b59
Riyan Dwi Cahyadi <114849739+RiyanAokii1@users.noreply.github.com>
2025-02-23 09:49:06 +0800

    also add nama to the table database

--- database/migrations/2025_01_06_084644_create_presensi_table.php
+++ database/migrations/2025_01_06_084644_create_presensi_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('presensi', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nama');
             $table->string('nomor_induk');
             $table->foreign('nomor_induk')->references('nomor_induk')->on('users')->onDelete('cascade');
             $table->date('tanggal_presensi');
