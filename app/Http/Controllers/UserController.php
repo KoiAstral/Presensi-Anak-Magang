9584a62ee8a4e669df5bd575f1984765a228d628
Riyan Dwi Cahyadi <114849739+RiyanAokii1@users.noreply.github.com>
2025-02-25 08:09:28 +0800

    fixed function for profile dashboard for user and admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $user = Auth::user();
         $users = User::all();
-        return view('admin.user.data_magang', compact('users')); // Sesuai dengan lokasi view
+        return view('admin.user.data_magang', compact('user', 'users')); // Sesuai dengan lokasi view
     }
 
     public function create()
